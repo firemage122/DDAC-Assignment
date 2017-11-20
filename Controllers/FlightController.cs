db416151b9bfc0106607c0501c7d00256c4c5a1e
firemage122 <firemage122@hotmail.com>
2017-11-20 18:24:57 +0800

    Add files via upload

---
+++ Controllers/FlightController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class FlightController : Controller
+    {
+        //create a StudentsDbContext object  
+        ASSIGNMENTEntities db = new ASSIGNMENTEntities();
+        public ActionResult SearchFlight()
+        {
+            List<Flight> FlightList = db.Flights.ToList();
+            return View(FlightList);
+
+        }
+        [HttpPost]
+        public ActionResult SearchFlight(String searcho, String searchd, String ddate)
+        {
+            
+            var Flightsearch = db.Flights.Where(x => x.Origin.Contains(searcho) && x.Destination.Contains(searchd) || searcho == null).ToList();
+
+            return View(Flightsearch);
+        }
+
+        public ActionResult BookConfirmation()
+        {
+            return View();
+        }
+    }
+
+}
